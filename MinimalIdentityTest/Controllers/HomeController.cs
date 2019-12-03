967e3b3e314e014f6bdd7eb89f6bccd3860c3d5e
mko <gogs@fake.local>
2019-12-03 11:58:30 +0100

    Add project files.

---
+++ MinimalIdentityTest/Controllers/HomeController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity.Owin;
+using System.Security.Claims;
+using MinimalIdentityTest.Models;
+
+namespace MinimalIdentityTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            var userStore = new UserStore<IdentityUser>();
+            IdentityUser[] identityUsers = userStore.Users.ToArray();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register(string userName, string password)
+        {
+            // Default UserStore constructor uses the default connection string named: DefaultConnection
+            var userStore = new UserStore<IdentityUser>();
+            var manager = new UserManager<IdentityUser>(userStore);
+
+            IdentityUser user = new IdentityUser() { UserName = userName };
+            IdentityResult result = manager.Create(user, password);
+
+            string r;
+            
+            if (result.Succeeded)
+            {
+                var authenticationManager = HttpContext.GetOwinContext().Authentication;
+                var userIdentity = manager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
+
+                r = string.Format("User {0} was created successfully!", user.UserName);
+            }
+            else
+            {
+                r = result.Errors.FirstOrDefault();
+            }
+
+            return Content(r);
+        }
+
+        [HttpGet]
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(string UserName, string Password)
+        {
+            UserStore<IdentityUser> userStore = new UserStore<IdentityUser>();
+            UserManager<IdentityUser> userManager = new UserManager<IdentityUser>(userStore);
+            var user = userManager.Find(UserName, Password);
+
+            if (user != null)
+            {
+                IAuthenticationManager authenticationManager = HttpContext.GetOwinContext().Authentication;
+                ClaimsIdentity userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = false }, userIdentity);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return Content("Invalid username or password.");
+            }
+        }
+
+        public ActionResult SignOut()
+        {
+            var authenticationManager = HttpContext.GetOwinContext().Authentication;
+            authenticationManager.SignOut();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult ChangePassword()
+        {
+            return View(new ChagePasswordVM());
+        }
+
+        [HttpPost]
+        public ActionResult ChangePassword(ChagePasswordVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                UserStore<IdentityUser> userStore = new UserStore<IdentityUser>();
+                UserManager<IdentityUser> userManager = new UserManager<IdentityUser>(userStore);
+                string userId = User.Identity.GetUserId<string>();
+
+                IdentityResult identityResult = userManager.ChangePassword(userId, model.CurrentPassword, model.NewPassword);
+
+                if (!identityResult.Succeeded)
+                {
+                    ModelState.AddModelError("", identityResult.Errors.First());
+                    return View(model);
+                }
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
---
+++ MinimalIdentityTest/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MinimalIdentityTest
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_PostAuthorizeRequest(object sender, EventArgs e)
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("pl");
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("pl-PL");
+        }
+
+    }
+}
